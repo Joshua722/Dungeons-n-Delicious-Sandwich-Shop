ba66746f2966a39d5e47db5de95c9ab51a70447c
Joshua <Joshua32149@gmail.com>
2023-11-27 14:01:25 -0500

    reciept writer

--- src/main/java/com/pluralsight/CustomerReceipt.java
+++ src/main/java/com/pluralsight/CustomerReceipt.java
@@ -5,32 +5,16 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-public abstract class CustomerReceipt implements CustomerOrder{
+public abstract class CustomerReceipt {
     public static void receiptReader()throws IOException{
-        String input;
-        LocalDate orderDate;
-        LocalTime orderTime;
-        String sandwichBread;
-        String sandwichToppings;
-        double orderAmount;
-        //create Readers
-        LocalDateTime receiptTime = null;
+        //create Writer
+        LocalDateTime receiptTime = LocalDateTime.now();
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd-hhmmss");
-        FileReader fileReader = new FileReader(receiptTime.format(dateTimeFormatter));
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        while((input = bufferedReader.readLine()) == null){
-         String [] receiptReader = input.split("\\ ");
-         orderDate = LocalDate.parse(receiptReader[0]);
-         orderTime = LocalTime.parse(receiptReader[1]);
-         sandwichBread = receiptReader[2];
-         sandwichToppings = receiptReader[3];
-         orderAmount = Double.parseDouble(receiptReader[4]);
-         //store into array list
-
-
+        String fileName = "src/main/Receipts/" + receiptTime.format(dateTimeFormatter) + ".txt";
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName));
+        bufferedWriter.write(fileName);
         }
-
     }
 
 
-}
+
